06f8594f38bca91e7ac91fc60b007f177316219d
sand-sea <70316064+sand-sea@users.noreply.github.com>
2023-04-12 22:17:35 +0800

    Update CheckIn.py

--- 01.Python/CheckIn.py
+++ 01.Python/CheckIn.py
@@ -68,6 +68,69 @@ def do_v2ray_check_in():
         print(f"签到失败!!!\r\n{check_in_json_data}")
 
 
+def do_Pork16_check_in():
+    """
+    Pork16 服务器签到获取流量
+    :return:
+    """
+    print("执行V2ray账号签到获取流量....")
+    login_url = "https://forever.pork16.com/auth/login"
+    headers = {
+        "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+        "DNT": "1",
+        "sec-ch-ua-mobile": '?0',
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+        "Content-Type": 'application/x-www-form-urlencoded; charset=UTF-8',
+        "Accept": 'application/json, text/javascript, */*; q=0.01',
+        "X-Requested-With": 'XMLHttpRequest',
+        "sec-ch-ua-platform": '"Windows"',
+        "Origin": 'https://forever.pork16.com',
+        "Sec-Fetch-Site": 'same-origin',
+        "Sec-Fetch-Mode": 'cors',
+        "Sec-Fetch-Dest": 'empty',
+        "Referer": 'https://forever.pork16.com/auth/login'
+    }
+    v2ray_server_user_name = os.environ['V2RAY_SERVER_USER_NAME']
+    v2ray_server_user_pwd = os.environ['V2RAY_SERVER_USER_PWD']
+    data_dict = {'email': v2ray_server_user_name, 'passwd': v2ray_server_user_pwd, 'code': ''}
+    # print(data_dict)
+    r = requests.post(login_url, data=data_dict, headers=headers)
+    print(r.text)
+
+    json_data = json.loads(r.text)
+    # 判断是否登录成功
+    if json_data.get('msg', '') != "登录成功":
+        print("登录失败")
+        return -1
+    else:
+        print("登录成功!!!!!!,开始执行签到工作")
+    check_in_url = "https://forever.pork16.com/user/checkin"
+    headers = {
+        "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+        "DNT": "1",
+        "sec-ch-ua-mobile": '?0',
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+        "Accept": 'application/json, text/javascript, */*; q=0.01',
+        "X-Requested-With": 'XMLHttpRequest',
+        "sec-ch-ua-platform": '"Windows"',
+        "Origin": 'https://forever.pork16.com',
+        "Sec-Fetch-Site": 'same-origin',
+        "Sec-Fetch-Mode": 'cors',
+        "Sec-Fetch-Dest": 'empty',
+        "Referer": 'https://forever.pork16.com/auth/login'
+    }
+    check_r = requests.post(check_in_url, headers=headers, cookies=r.cookies)
+    check_in_json_data = json.loads(check_r.text)
+    print(check_in_json_data)
+    if check_in_json_data['msg'] == "您似乎已经签到过了...":
+        print(f"已签到{check_in_json_data}")
+        return 1
+    elif check_in_json_data['msg'].startswith("获得了"):
+        print(f"签到成功!!!\r\n{check_in_json_data}")
+    else:
+        print(f"签到失败!!!\r\n{check_in_json_data}")
+
+ 
 def netease_cloud_pc_sign_in():
     print("执行网易云音乐PC端签到....")
     str_cookies = os.environ['NETEASE_CLOUD_MUSIC_PC_SIGN_COOKIES']
@@ -98,4 +161,5 @@ def netease_cloud_pc_sign_in():
 
 if __name__ == "__main__":
     do_v2ray_check_in()
+    do_Pork16_check_in()
     # netease_cloud_pc_sign_in()
